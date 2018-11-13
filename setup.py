bca639f74d8ba3eaceb42125ec7bac890e41cdb6
Abdel <abdel.elbel@gmail.com>
2018-11-13 23:45:12 +0100

    cleaning repo

--- setup.py
+++ setup.py
@@ -10,44 +10,24 @@ import bouygues_pysms
 
 here = os.path.abspath(os.path.dirname(__file__))
 
-#def read(*filenames, **kwargs):
-#    encoding = kwargs.get('encoding', 'utf-8')
-#    sep = kwargs.get('sep', '\n')
-#    buf = []
-#    for filename in filenames:
-#        with io.open(filename, encoding=encoding) as f:
-#            buf.append(f.read())
-#    return sep.join(buf)
-
-#long_description = read('README.txt', 'CHANGES.txt')
-
-#class PyTest(TestCommand):
-#    def finalize_options(self):
-#        TestCommand.finalize_options(self)
-#        self.test_args = []
-#        self.test_suite = True
-#
-#    def run_tests(self):
-#        import pytest
-#        errcode = pytest.main(self.test_args)
-#        sys.exit(errcode)
-
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except(IOError, ImportError, OSError):
+    long_description = open('README.md').read()
+    
 setup(
     name='bouygues_pysms',
     version=bouygues_pysms.__version__,
     url='https://github.com/abdel-elbel/bouygues_pysms/',
-#    license=io.open('LICENSE', encoding='utf-8').read().encode("utf-8"),
-    license='MIT License',
+    license=io.open('LICENSE', encoding='utf-8').read().encode("utf-8"),
     author='Abdel El Bel',
-#    tests_require=['pytest'],
     install_requires=['requests',
-                     're',
+                     're'
                      ],
-#    cmdclass={'test': PyTest},
     author_email='abdel.elbel@gmail.com',
-    description='bouygues_pysms is a Python interface to Bouygues Mobile SMS API',
-#    long_description=long_description,
-    long_description='Send SMS with Free Mobile',
+    description='This module is a Python interface to Bouygues Mobile SMS API',
+    long_description=long_description,
     packages=['bouygues_pysms'],
     include_package_data=True,
     platforms='any',
@@ -64,7 +44,5 @@ setup(
         'Topic :: Software Development :: Libraries :: Application Frameworks',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
         ],
-#    extras_require={
-#        'testing': ['pytest'],
-#    }
+
 )
